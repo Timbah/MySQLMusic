d52771402ed60874ac9a2e8171f19469d3400077
Thembelani Ngema <thembelaningema20@gmail.com>
2025-10-25 09:09:22 +0200

    Connecting to the music database using the driver manager class

--- src/main/java/thembelani/java/tutorials/Main.java
+++ src/main/java/thembelani/java/tutorials/Main.java
@@ -1,17 +1,31 @@
 package thembelani.java.tutorials;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+public class Main {
+
+    private final static String CONN_STRING = "jdbc:mysql://localhost:3306/music";
+
+    public static void main(String[] args) {
+
+        String username = JOptionPane.showInputDialog(null, "Enter DB Username");
+
+        JPasswordField pf = new JPasswordField();
+
+        int okCxl = JOptionPane.showConfirmDialog(null, pf, "Enter DB Password", JOptionPane.OK_CANCEL_OPTION);
+
+        final char[] password = (okCxl == JOptionPane.OK_OPTION) ? pf.getPassword() : null;
+
+        try (Connection connection = DriverManager.getConnection(CONN_STRING, username, String.copyValueOf(password))) {
+            System.out.println("Success!! Connection made to the music database");
+            Arrays.fill(password, ' '); //Clearing the password to keep it in memory for as short at possible
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }//Connection automatically gets closed once the try-catch block is completed, so no need to explicitly close it
     }
 }
